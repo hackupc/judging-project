db064895045bf172e5fed4886887f062e61781d5
Carlota Catot Bragós <carkbra@gmail.com>
2021-10-10 13:02:31 +0200

    Created loginform component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form">
+    <img alt="Vue logo" src="../assets/logo.png" style="width: 20%" />
+    <div>
+      <span>Sala de judging</span>
+      <input v-model="message" >
+    </div>
+    <div>
+      <span>Password</span>
+      <input v-model="message" type="password" >
+    </div>
+    <button type="submit">Sign In</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+};
+</script>
+
+<style scoped></style>
