fdb196e77bba9d96a050147d47869a3cfe317626
Carlota Catot Bragós <carkbra@gmail.com>
2021-10-14 17:33:22 +0200

    added back button

--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="back-button">
+    <button class="back-button__button">Back to all projects</button>
+  </div>
   <div class="information">
     <ProjectInformation />
     <PunctuationCard />
@@ -18,5 +21,20 @@ export default {
 .information {
   display: flex;
   justify-content: space-around;
+}
+.back-button {
+  text-align: start;
+  padding: 0 30px 30px 30px;
+
+  .back-button__button {
+    background: #e94662;
+    border-radius: 28px;
+    color: #ffffff;
+    text-transform: uppercase;
+    border: none;
+    padding: 10px 30px;
+    cursor: pointer;
+    font-size: 20px;
+  }
 }
 </style>
