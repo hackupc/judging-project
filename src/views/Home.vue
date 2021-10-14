c410be858785126b2ba449da85401c2e50c028b0
Carlota Catot Bragós <carkbra@gmail.com>
2021-10-15 00:43:07 +0200

    deleted unnecesary file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,11 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue";
 import ProjectsTable from "@/components/ProjectsTable.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     ProjectsTable,
-    HelloWorld,
   },
 })
 export default class Home extends Vue {}
