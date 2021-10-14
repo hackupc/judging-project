277b424f44d250ca04ce790b5bf7814e207515c0
Carlota Catot Bragós <carkbra@gmail.com>
2021-10-14 17:21:46 +0200

    changed styles of project information

--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,16 +1,22 @@
 <template>
-  <p>This will be the project page</p>
-  <div>
+  <div class="information">
+    <ProjectInformation />
     <PunctuationCard />
   </div>
 </template>
 
 <script>
 import PunctuationCard from "../components/PunctuationCard";
+import ProjectInformation from "../components/ProjectInformation";
 export default {
   name: "ProjectPage",
-  components: { PunctuationCard },
+  components: { ProjectInformation, PunctuationCard },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.information {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
